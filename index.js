1c585db401bd5f45264f04632f2241827e457c86
Samleo <ec3team@gmail.com>
2018-10-14 16:15:21 +0800

    added hi

--- index.js
+++ index.js
@@ -10,8 +10,12 @@ const Telegraf = require('telegraf');
 const TELE_TOKEN = "633536414:AAENJwkQwYN3TGOe3LmFw2VyJFr8p7dU9II";
 const app = new Telegraf(TELE_TOKEN);
 
-app.hears('hi', ctx => {
-  return ctx.reply('Hey!');
+app.hears('/(hi|hello|hey|shalom)+/gi', ctx => {
+  return ctx.reply('Shalom!');
 });
+
+app.catch((err) => {
+  console.log('Ooops', err)
+})
 
 app.startPolling();
