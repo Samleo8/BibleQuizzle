7c57ba542a5c1541552009758995e95252c75b79
Samleo <ec3team@gmail.com>
2018-10-26 16:36:20 +0800

    Added welcome and help messages

--- index.js
+++ index.js
@@ -6,18 +6,39 @@ REFERENCES:
 - https://www.sohamkamani.com/blog/2016/09/21/making-a-telegram-bot/
 */
 
-const Telegraf = require('telegraf');
-const TELE_TOKEN = "633536414:AAENJwkQwYN3TGOe3LmFw2VyJFr8p7dU9II";
-const app = new Telegraf(TELE_TOKEN);
+/*
+const BOT_TOKEN = "633536414:AAENJwkQwYN3TGOe3LmFw2VyJFr8p7dU9II";
+const app = new Telegraf(BOT_TOKEN);
+*/
+//RUN now as follows: now -e BOT_TOKEN='633536414:AAENJwkQwYN3TGOe3LmFw2VyJFr8p7dU9II' --public
 
-const regex_hi = new RegExp("(hi|hello|hey|shalom)","gi");
-app.hears(regex_hi, ctx => {
-  return ctx.reply('Shalom!');
+const { Extra } = require('micro-bot');
+const Telegraf  = require('micro-bot');
+const bot = new Telegraf(process.env.BOT_TOKEN);
+
+const welcomeMessage = 'Welcome to Bible Quizzle, a fast-paced Bible trivia game similar to Quizzarium!\n\nTo begin the game, type /start in the bot\'s private chat, or in the group. For more information and a list of all commands, type /help';
+
+const helpMessage =
+"Bible Quizzle is a fast-paced Bible trivia game. Once the game is started, the bot will send a question. Send in your open-ended answer and the bot will give you points for the right answer. The faster you answer, the more points you get! You can use hints but that costs points.\n\n"+
+"/start Starts a new game.\n"+
+"/help Displays this help message.\n";
+
+let currentGame = {};
+let scores = {};
+let welcomeMessageSent = false;
+
+bot.start( (ctx) => {
+	if(!welcomeMessageSent){
+		ctx.reply(welcomeMessage);
+		welcomeMessageSent = true;
+	}
+
+	console.log("Game started. ", ctx);
 });
 
-app.catch((err) => {
-  console.log('Ooops', err);
-  return ctx.reply('Ooops:'+err);
+bot.command('help', ctx => {
+	//const extra = Object.assign({}, Composer.markdown());
+	ctx.reply(helpMessage);
 })
 
-app.startPolling();
+module.exports = bot;
